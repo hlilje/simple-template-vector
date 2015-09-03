b041e0ca50984e06f196e13b544ea15618ccc0df
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-09-03 11:06:50 +0200

    Add header functions

--- vector.h
+++ vector.h
@@ -3,4 +3,21 @@ class Vector
 {
     private:
     public:
+        void push_back(T);
+
+        void insert(std::size_t, T);
+
+        void clear();
+
+        void erase(std::size_t);
+
+        std::size_t size();
+
+        std::size_t capacity();
+
+        begin();
+
+        TODO end();
+
+        TODO find(const T&);
 };
