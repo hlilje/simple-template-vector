50ee672ece94defc7eacc46649a9055b8b0524a2
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-09-08 09:46:05 +0200

    Add constructors

--- iterator.h
+++ iterator.h
@@ -6,4 +6,13 @@ class VectorIterator : public std::iterator<std::random_access_iterator_tag, T>
 {
     public:
         VectorIterator();
+
+        VectorIterator(const VectorIterator&);
+
+        ~VectorIterator();
+
+
+        VectorIterator& operator=(const VectorIterator&);
+
+        VectorIterator& operator=(VectorIterator&&);
 };
